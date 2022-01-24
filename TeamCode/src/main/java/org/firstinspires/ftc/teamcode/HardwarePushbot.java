3f057a3d2af9f87ea525fd05753574ef804a9586
Yaseen Maqsudi <yaseenmaqsudi@gmail.com>
2022-01-23 20:20:48 -0500

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
@@ -0,0 +1,99 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwarePushbot
+{
+    /* Public OpMode members. */
+    public DcMotor right;
+    public DcMotor left;
+
+    public DcMotor arm;
+    public DcMotor intake;
+
+    public Servo spinner;
+
+
+
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwarePushbot(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        right  = hwMap.get(DcMotor.class, "right");
+        left = hwMap.get(DcMotor.class, "left");
+
+        arm = hwMap.get(DcMotor.class, "arm");
+        intake = hwMap.get(DcMotor.class, "intake");
+
+        spinner = hwMap.get(Servo.class, "spinner");
+
+
+        right.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        // Set all motors to zero power
+        right.setPower(0);
+        left.setPower(0);
+        arm.setPower(0);
+        intake.setPower(0);
+
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+
+        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+ }
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -0,0 +1,128 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+@Autonomous(name="auto", group="Linear Opmode")
+public class auto extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    HardwarePushbot hardware = new HardwarePushbot();
+
+    final int ticks = 500;
+    double circumference = 3.14*2.5;
+
+    @Override
+    public void runOpMode() {
+        hardware.init(hardwareMap);
+
+        hardware.left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hardware.right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // pick arm up so it doesn't drag
+            // go forward 10 inches
+            // intake ball
+            // turn 90 ccw
+            // drive 10 inches
+            // arm up
+            // outtake on top layer
+
+            armUp();
+            driveForwardOrBackward(.2, 10);
+            intake(2);
+
+
+
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+
+
+
+    public void armUp() {
+        hardware.arm.setPower(.5);
+        sleep(500);
+        hardware.arm.setPower(0.1);
+    }
+
+    public void driveForwardOrBackward(double power, double distance) {
+        hardware.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hardware.right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        double rotationsNeeded = distance/circumference;
+        int encoderDrivingTarget = (int) (rotationsNeeded * ticks);
+
+        hardware.left.setTargetPosition(encoderDrivingTarget);
+        hardware.right.setTargetPosition(encoderDrivingTarget);
+
+        hardware.left.setPower(power);
+        hardware.right.setPower(power);
+
+        hardware.left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        hardware.right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (hardware.left.isBusy() || hardware.right.isBusy()) {
+            // do nothing while running
+            telemetry.addData("Path", "Driving");
+        }
+
+        hardware.left.setPower(0);
+        hardware.right.setPower(0);
+    }
+
+    public void intake(int time) {
+        hardware.intake.setPower(1);
+        sleep(time * 1000);
+    }
+}
